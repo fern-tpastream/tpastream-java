f25af3ce5a2af0c5b4a08631892db2626e0b8f46
dsinghvi <deep@usebirch.com>
2022-10-21 08:47:52 -0700

    commit sample app

---
+++ sample-app/src/main/java/sample/App.java
@@ -0,0 +1,13 @@
+package sample;
+
+import com.tpastream.api.client.TpastreamApiClient;
+
+public class App {
+
+    public static void main(String... args) {
+
+        TpastreamApiClient tpastreamApiClient =
+                new TpastreamApiClient("https://app.tpastream.com/api/");
+
+    }
+}
